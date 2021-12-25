72b301c8575b25d8e9df88edfac2bd11dff5e5e0
Sergii Zhuravel <szhuravell@gmail.com>
2021-12-25 23:11:44 +0200

    Fixed sentinel linear

--- algo_unlocked/JavaScript/chapter2/ch2__sentinel_linear.js
+++ algo_unlocked/JavaScript/chapter2/ch2__sentinel_linear.js
@@ -1,16 +1,17 @@
 // LINEAR-SEARCH(A, n, x)
 
 function sentinelLinearSearch(a, n, x) {
-  const last = a[n - 1];
-  a[n - 1] = x;
+  const lastIdx = n - 1;
+  const last = a[lastIdx];
+  a[lastIdx] = x;
 
   let i = 0;
   while (a[i] !== x) {
     i++;
   }
 
-  a[n - 1] = last;
-  if (i < n || a[n - 1] === x) {
+  a[lastIdx] = last;
+  if (i < lastIdx || a[lastIdx] === x) {
     return i;
   }
 
@@ -18,9 +19,9 @@ function sentinelLinearSearch(a, n, x) {
 }
 
 const arr = [2, 3, 4, 5];
-const res1 = sentinelLinearSearch(arr, arr.length, 5);
+const res1 = sentinelLinearSearch(arr, arr.length, 3);
 const res2 = sentinelLinearSearch(arr, arr.length, 6);
 console.log("Res1: ", res1);
 console.log("Res2: ", res2);
-console.log("Test 1: ", res1 === 3);
+console.log("Test 1: ", res1 === 1);
 console.log("Test 2: ", res2 === null);
